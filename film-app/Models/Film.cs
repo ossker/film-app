cac4b268449ef8d603b5d561a9d5c50b727dbaac
ossker <oskar.bienko@onet.pl>
2023-02-21 17:04:30 +0100

    Film model added.

---
+++ film-app/Models/Film.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace film_app.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Wpisz tytuł")]
+        [StringLength(50)]
+        public string Tytul { get; set; }
+
+        [Required(ErrorMessage = "Wpisz reżysera")]
+        [StringLength(50)]
+        public string Rezyser { get; set; }
+
+        [Required(ErrorMessage = "Wpisz opis")]
+        [StringLength(200)]
+        public string Opis { get; set; }
+        public decimal? Cena { get; set; }
+        public DateTime DataDodania { get; set; }
+
+    }
+}

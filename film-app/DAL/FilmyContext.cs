72f528827b6614b58c7bc2d735a9d46b9c022cb9
ossker <oskar.bienko@onet.pl>
2023-02-27 10:06:47 +0100

    Context class added.

---
+++ film-app/DAL/FilmyContext.cs
@@ -0,0 +1,18 @@
+﻿using film_app.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace film_app.DAL
+{
+    public class FilmyContext:DbContext
+    {
+       
+        public DbSet<Film> Filmy { get; set; }
+        public DbSet<Kategoria> Kategorie { get; set; }
+
+        public FilmyContext(DbContextOptions<FilmyContext> options) : base(options)
+        {
+
+        }
+
+    }
+}

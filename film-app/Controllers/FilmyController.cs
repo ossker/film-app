9eeb060ee66e0d6bee802a244c822d22870f53c8
ossker <oskar.bienko@onet.pl>
2023-03-13 10:35:06 +0100

    FilmyController added in order to query all films. View with these films created.

---
+++ film-app/Controllers/FilmyController.cs
@@ -0,0 +1,25 @@
+﻿using film_app.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace film_app.Controllers
+{
+    public class FilmyController : Controller
+    {
+        FilmyContext db;
+
+        public FilmyController(FilmyContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Lista(string nazwaKategorii)
+        {
+            var kategoria = db.Kategorie.Include("Filmy").Where(k=>k.Nazwa.ToUpper() == nazwaKategorii).Single();
+            var filmy = kategoria.Filmy.ToList();
+            ViewBag.nazwa = nazwaKategorii;
+            return View(filmy);
+        }
+     }
+}
